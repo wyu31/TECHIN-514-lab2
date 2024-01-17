351225fe6e1a97c05404977e41cae63799d3fb94
Wanling Yu <wyu31@uw.edu>
2024-01-16 19:09:33 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#define ANALOG_PIN 1
+#include <Arduino.h>
+
+
+void setup() {
+  Serial.begin(115200); // Start the serial communication
+}
+
+void loop() {
+  int adcValue = analogRead(ANALOG_PIN); // Read the analog value
+  float voltage = (adcValue / 4095.0) * 3.3; // Convert ADC value to voltage
+
+  Serial.print("Voltage: ");
+  Serial.print(voltage);
+  Serial.println(" V");
+
+  delay(1000); // Wait for 1 second before reading again
+}
